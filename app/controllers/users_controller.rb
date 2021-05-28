8f3f9744b9e1cd1d1e62a9da3d0bcbdba6e04fa6
MdSulaiman2k <mdsulaiman2k@gmail.com>
2021-05-29 02:30:53 +0530

    add todos-controller and users_controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,39 @@
+class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def index
+    render plain: User.order(:id).map { |user| user.to_pleasent_string }.join("\n")
+  end
+
+  def show
+    id = params[:id]
+    user = User.find(id)
+    render plain: user.to_pleasent_string
+  end
+
+  def create
+    name = params[:name]
+    email = params[:email]
+    password = params[:password]
+    user = User.create!(
+      name: name,
+      email: email,
+      password: password,
+    )
+    response_text = "You are registered successfully with an id #{user.id}"
+    render plain: response_text
+  end
+
+  def login
+    email = params[:email]
+    password = params[:password]
+    user = User.where("email = ?", email).first
+    if user == nil
+      render plain: "false"
+    elsif user.password == password
+      render plain: "true"
+    else
+      render plain: "false"
+    end
+  end
+end
