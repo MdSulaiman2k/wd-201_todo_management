9d48927abbe0c44732848521010d3c873eef008d
MdSulaiman2k <mdsulaiman2k@gmail.com>
2021-06-02 16:54:29 +0530

    Add user sign-in function (without session)

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/" => "home#index"
   resources :todos
+  get "/signin" => "sessions#new", as: :new_sessions
+  post "/signin" => "sessions#create", as: :sessions
   post "users/login", to: "users#login"
   resources :users
 end
