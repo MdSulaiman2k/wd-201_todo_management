0ccb8bbd4fc1e5b96026463256d369296eab3c38
md Sulaiman <77278345+MdSulaiman2k@users.noreply.github.com>
2021-06-13 09:31:24 +0530

    remove unused code

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -16,21 +16,6 @@ class Todo < ActiveRecord::Base
     where("due_date> ?", Date.today).order(:due_date, :id)
   end
 
-  def self.show_list
-    puts "My Todo-list\n\n"
-    puts "Overdue\n"
-    puts overdue.map { |todo| todo.to_displayable_string }
-
-    puts "\n\n"
-    puts "Due Today\n"
-    puts due_today.map { |todo| todo.to_displayable_string }
-    puts "\n\n"
-
-    puts "Due Later\n"
-    puts due_later.map { |todo| todo.to_displayable_string }
-    puts "\n\n"
-  end
-
   def self.completed
     all.where(completed: true)
   end
